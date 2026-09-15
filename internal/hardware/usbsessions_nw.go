2e5e9d244cf68cfa0f8f77fbe876dbca26bb95a1
Nguyễn Hữu Cường - TC22B <108710596+nguyenhuucuongTC21B@users.noreply.github.com>
2026-09-15 18:12:55 +0700

    Delete internal/hardware/usbsessions_nw.go

--- internal/hardware/usbsessions_nw.go
+++
@@ -1,9 +0,0 @@
-//go:build !windows
-
-package hardware
-
-// scanDeviceSessions trên non-Windows không truy vết được Event Log -
-// lịch sử session đã được nhúng sẵn trong demo data của usb_nw.go.
-func scanDeviceSessions() map[string]*DeviceSessions {
-        return map[string]*DeviceSessions{}
-}
